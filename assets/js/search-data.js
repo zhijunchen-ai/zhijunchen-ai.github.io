0dd234eb9d10117b2f011ad1545398e0e54cbd15
zhijunchen-ai <zhijunchen.cs@gmail.com>
2025-08-14 19:20:46 +0000

    Deploying to gh-pages from @ zhijunchen-ai/zhijunchen-ai.github.io@f508a1315dc9934eba9934d3733abe32d7710ada 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-services",
+          title: "services",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/service/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
