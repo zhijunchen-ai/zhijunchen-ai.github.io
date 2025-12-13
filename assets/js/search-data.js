a40d090c7c8fe7fb7abeac283f78bbfe07162407
zhijunchen-ai <zhijunchen.cs@gmail.com>
2025-12-13 19:46:04 +0000

    Deploying to gh-pages from @ zhijunchen-ai/zhijunchen-ai.github.io@e71df0581ac04cb7f5e15f95c66df02b5d4c05b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-about",
-          title: "About",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-papers",
+  },{id: "nav-papers",
           title: "Papers",
           description: "",
           section: "Navigation",
